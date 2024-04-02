b6d716197eccd0f05f1efece76d3dc061ef7ea16
Nirmada Edirisinghe <134194456+nirmada001@users.noreply.github.com>
2024-04-02 13:13:55 +0530

    Delete js directory

--- js/events.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.inqbtn').onclick = () =>{
-    document.querySelector('.inquirepopup').style.display = 'block';
-    document.querySelector('.event-image').style.display = 'none';
-}
-
-document.querySelector('.popupclose').onclick = () =>{
-    document.querySelector('.inquirepopup').style.display = 'none';
-    document.querySelector('.event-image').style.display = 'block';
-}
